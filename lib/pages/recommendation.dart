42079b5cec0dac1445c4c9addf8d65391d33b2a1
kensunjaya <kennethsunjaya@gmail.com>
2025-06-03 19:59:44 +0700

    set up theme

--- lib/pages/recommendation.dart
+++ lib/pages/recommendation.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:rebio/theme/constants.dart';
 
 class Recommendation extends StatefulWidget {
   const Recommendation({super.key});
@@ -12,6 +14,7 @@ class _RecommendationState extends State<Recommendation> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: bg,
       body: Padding(
         padding: EdgeInsets.only(top: 64.0),
         child: Center(
@@ -29,10 +32,87 @@ class _RecommendationState extends State<Recommendation> {
                   child: Text("Time to give your Eco Enzyme some care!", style: GoogleFonts.notoSans(fontSize: 18), textAlign: TextAlign.center,)
                 )
               ),
-              Column(
-                children: [
-                  
-                ]
+              Padding(
+                padding: EdgeInsets.only(left: 24, right: 24),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Today",
+                      style: GoogleFonts.notoSans(fontSize: 16, fontWeight: FontWeight.w500),
+                      textAlign: TextAlign.left,
+                    ),
+                    Padding(padding: EdgeInsets.only(bottom: 8.0)),
+                    SizedBox(
+                      child: Container(
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withValues(
+                                alpha: 0.5,
+                              ),
+                              spreadRadius: 1,
+                              blurRadius: 5,
+                              offset: Offset(0, 3), // changes position of shadow
+                            ),
+                          ],
+                        ),
+                        child: Padding(
+                          padding: EdgeInsets.all(16),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Row(
+                                    children: [
+                                      Text(
+                                        "Inspect for mold growth.",
+                                        style: GoogleFonts.notoSans(fontSize: 16),
+                                        textAlign: TextAlign.left,
+                                      ),
+                                      Padding(
+                                        padding: EdgeInsets.only(left: 8.0),
+                                        child: Text(
+                                          "+3",
+                                          style: GoogleFonts.notoSans(fontSize: 16, color: yellowPoint),
+                                        )
+                                      )
+                                      
+                                    ]
+                                  ),
+                                  
+                                  Padding(
+                                    padding: EdgeInsets.only(top: 8.0),
+                                    child: Opacity(
+                                      opacity: 0.5,
+                                      child: Text(
+                                        "2 hours ago",
+                                        style: GoogleFonts.notoSans(fontSize: 14),
+                                        textAlign: TextAlign.left,
+                                      ),
+                                    ),
+                                  )
+                                ],
+                              ),
+                              SvgPicture.asset(
+                                'assets/mold.svg',
+                                colorFilter: ColorFilter.mode(
+                                  primary,
+                                  BlendMode.srcIn,
+                                )
+                              ),
+                            ]
+                          )
+                        ),
+                      )
+                    )
+                  ]
+                )
               )
             ]
           )
---
+++ lib/theme/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const Color bg = Color(0xFFEBF2F2);
+const Color primary = Color(0xFF3C7C54);
+const Color yellowPoint = Color(0xFFE4A000);
