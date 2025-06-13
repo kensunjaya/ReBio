891055acb6dd5154d8f7f9e71560ecde0537ac26
Lavelinee <novellina.e@gmail.com>
2025-06-13 23:37:01 +0700

    Create login page

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -40,48 +40,123 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Login')),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              controller: emailController,
-              decoration: const InputDecoration(
-                labelText: 'Email',
-                border: OutlineInputBorder(),
+      // appBar: AppBar(title: const Text('Login')),
+      backgroundColor: Color(0xFFF1F5F9),
+      body: Center(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                'Login',
+                style: TextStyle(fontSize: 28,fontWeight: FontWeight.bold),
               ),
-            ),
-            const SizedBox(height: 16),
-            TextField(
-              controller: passwordController,
-              obscureText: true,
-              decoration: const InputDecoration(
-                labelText: 'Password',
-                border: OutlineInputBorder(),
+              // Username Box
+              const SizedBox(height: 24),
+              TextFormField(
+                //controller: emailController,
+                decoration: const InputDecoration(
+                  labelText: 'Username/Email',
+                  hintText: 'Please input your username or email',
+                  border: OutlineInputBorder(),
+                ),
               ),
-            ),
-            const SizedBox(height: 24),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () => login(context),
-                child: const Text('Login'),
+              
+              //Password Box
+              const SizedBox(height: 16),
+              TextField(
+                controller: passwordController,
+                obscureText: true,
+                decoration: const InputDecoration(
+                  labelText: 'Password',
+                  hintText: 'must be 8 characters',
+                  border: OutlineInputBorder(),
+                ),
               ),
-            ),
-            const SizedBox(height: 16),
-            TextButton(
-              onPressed: () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(builder: (context) => const RegisterPage()),
-                );
-              },
-              child: const Text("Don't have an account? Register"),
-            ),
-          ],
-        ),
+              // Forgot Password
+              const SizedBox(height: 8),
+              Align(
+                alignment: Alignment.centerRight,
+                child: TextButton(
+                  onPressed: () {},
+                  child: Text('Forgot password?',
+                  style: TextStyle(color: Colors.black),
+                  ),
+                ),
+              ),
+
+              //Login Button
+              const SizedBox(height: 24),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () => login(context),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFF10493F),
+                    padding: const EdgeInsets.symmetric(vertical: 24),
+                  ),
+                  child: const Text(
+                    'Log in',
+                    style: TextStyle(fontSize: 16, color: Colors.white),
+                    ),                  
+                ),
+              ),
+
+              //Other option to log in
+              const SizedBox(height: 16),
+              Row(
+                children: [
+                  Expanded(child: Divider()),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8),
+                    child: Text('Or log in with'),
+                    ),
+                    Expanded(child: Divider()),
+                ],
+              ),
+
+              const SizedBox(height: 16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  OutlinedButton(
+                    onPressed: () {},
+                    child: Image.asset(
+                      'assets/images/google_logo.png',
+                      height: 24,                    
+                    ),
+                  ),
+                  OutlinedButton(
+                    onPressed: () {},
+                    child: Icon(Icons.facebook, color: Colors.blue),
+                  ),
+                  OutlinedButton(
+                    onPressed: () {},
+                    child: Icon(Icons.apple),
+                  ),
+                ],
+              ),
+
+              const SizedBox(height: 16),
+              TextButton(
+                onPressed: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => const RegisterPage()),
+                  );
+                },
+                child: const Text(
+                  "Don't have an account? Sign up",
+                  style: TextStyle(
+                    color: Colors.black, fontWeight: FontWeight.bold
+                  ),
+                ),
+              ),
+            ],
+          )
+        )
+        ,
       ),
     );
   }
